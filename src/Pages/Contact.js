1e55632a596af4ce2d13689b4667234b57c47057
mavericabhi <43420996+mavericabhi@users.noreply.github.com>
2020-12-06 18:29:20 +0530

    Add files via upload

---
+++ src/Pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import buttonstyle from '../Components/button.module.css'
+
+
+const Contact = () => {
+    return (
+        <div>
+            <p1>gmail id: abhimish3115@gmail.com </p1> 
+            <button className={buttonstyle.btn}> <a href="https://www.linkedin.com/in/abhishek-mishra-0902ba171">My LinkedIn Profile</a> </button>   
+            <button className={buttonstyle.btn}> <a href="https://github.com/mavericabhi">My Github Profile</a> </button>
+            <button className={buttonstyle.btn}> <a href="https://www.instagram.com/abhi_mish3107/">My Instagram Handle</a> </button>
+        </div>
+    )
+}
+
+
+export default Contact
